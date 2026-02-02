09a9d7a5732ee9259e7260766ed0758c1414c88e
rudra786867 <hottarudraprasad2006@gmail.com>
2026-02-02 18:53:06 +0530

    remove gmail

--- app.py
+++ app.py
@@ -2,22 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-import smtplib
-EMAIL_ADDRESS = 'rudraprasadritu2006@gmail.com'
-EMAIL_PASSWORD = 'tveb ivfq ngro kwhv'
-RECIPIENTS = 'rudraprasadritu2006@gmail.com'
-def send_email(name):
-    subject = "New login name submitted"
-    body = f"Someone signed in with name: {name}"
-    message = f"Subject: {subject}\n\n{body}"
-    try:
-        server = smtplib.SMTP("smtp.gmail.com", 2525)
-        server.starttls()
-        server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        server.sendmail(EMAIL_ADDRESS, RECIPIENTS, message)
-        server.quit()
-    except Exception as e:
-        print("Email error:", e)
+
 
 
 
@@ -38,18 +23,16 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        name = form.user_id.data
+
         session['authenticated'] = True
 
-        try:
-            send_email(name)
-        except Exception as e:
-            print("Email failed:", e)
-            # still let the user continue
+
 
         return redirect(url_for('secret'))
+    else:
+        return render_template('login.html', form=form)
 
-    return render_template('login.html', form=form)
+
 
 
 
